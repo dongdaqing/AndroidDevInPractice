5d0f2c3d79248212dc6232f8b2e8d785cc850cb3
dongdaqing <ddaqing@gmail.com>
2014-09-16 13:06:47 +0800

    add action bar icon

--- ActionBar/WeChatStudy/gen/com/example/wechatstudy/R.java
+++ ActionBar/WeChatStudy/gen/com/example/wechatstudy/R.java
@@ -22,10 +22,32 @@ public final class R {
         public static final int activity_vertical_margin=0x7f040001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int actionbar_add_icon=0x7f020000;
+        public static final int actionbar_item_normal=0x7f020001;
+        public static final int actionbar_item_pressed=0x7f020002;
+        public static final int actionbar_more_icon=0x7f020003;
+        public static final int actionbar_search_icon=0x7f020004;
+        public static final int ic_launcher=0x7f020005;
+        public static final int logo=0x7f020006;
+        public static final int ofm_add_icon=0x7f020007;
+        public static final int ofm_camera_icon=0x7f020008;
+        public static final int ofm_card_icon=0x7f020009;
+        public static final int ofm_collect_icon=0x7f02000a;
+        public static final int ofm_feedback_icon=0x7f02000b;
+        public static final int ofm_group_chat_icon=0x7f02000c;
+        public static final int ofm_photo_icon=0x7f02000d;
+        public static final int ofm_qrcode_icon=0x7f02000e;
+        public static final int ofm_setting_icon=0x7f02000f;
+        public static final int ofm_video_icon=0x7f020010;
     }
     public static final class id {
-        public static final int action_settings=0x7f080000;
+        public static final int action_album=0x7f080002;
+        public static final int action_card=0x7f080004;
+        public static final int action_collection=0x7f080003;
+        public static final int action_feed=0x7f080006;
+        public static final int action_plus=0x7f080001;
+        public static final int action_search=0x7f080000;
+        public static final int action_settings=0x7f080005;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -34,9 +56,20 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050002;
+        public static final int action_album=0x7f050004;
+        public static final int action_card=0x7f050006;
+        public static final int action_collection=0x7f050005;
+        public static final int action_feed=0x7f050008;
+        public static final int action_plus=0x7f050003;
+        public static final int action_search=0x7f050002;
+        public static final int action_settings=0x7f050007;
         public static final int app_name=0x7f050000;
         public static final int hello_world=0x7f050001;
+        public static final int plus_add_friend=0x7f05000a;
+        public static final int plus_group_chat=0x7f050009;
+        public static final int plus_scan=0x7f05000c;
+        public static final int plus_take_photo=0x7f05000d;
+        public static final int plus_video_chat=0x7f05000b;
     }
     public static final class style {
         /** 
---
+++ ActionBar/WeChatStudy/src/com/example/wechatstudy/PlusActionProvider.java
@@ -0,0 +1,20 @@
+package com.example.wechatstudy;
+
+import android.content.Context;
+import android.view.ActionProvider;
+import android.view.View;
+
+public class PlusActionProvider extends ActionProvider{
+
+	public PlusActionProvider(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public View onCreateActionView() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
